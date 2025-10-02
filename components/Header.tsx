a9f7d211941790c5e249ddc58074852d7b7642f0
shipptim-sys <shipp.tim@googlemail.com>
2025-10-02 10:45:10 +0100

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import Container from "../Container";
+import Container from "@/components/Container";
 
 export default function Header() {
   return (
